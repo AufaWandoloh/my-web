eb22b976980ff5f0d2becff9d48104a965893848
Alpha <aufawandoloh123@gmail.com>
2025-02-16 22:24:46 +0700

    add style.css

--- recipe_manager/app.py
+++ recipe_manager/app.py
@@ -1,11 +1,20 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 
 @app.route("/")
 def home():
-    return "Hello, Recipe Manager!"
+    return """ 
+    <html>
+    <head>
+        <link rel='stylesheet' href='/static/style.css'>
+    </head>
+    <body>
+        <h1>Hello, Recipe Manager!</h1>
+    </body>
+    </html>
+    """
 
 
 if __name__ == "__main__":
