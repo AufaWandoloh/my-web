3bb1f5ae1711c4ad3927890bcc345f9c8d752ce6
Alpha <aufawandoloh123@gmail.com>
2025-02-17 17:23:02 +0700

    add index for amend ui, update app.py and login.html

--- recipe_manager/app.py
+++ recipe_manager/app.py
@@ -1,29 +1,16 @@
-from flask import Flask, render_template, redirect, url_for, flash
-from flask_wtf.csrf import CSRFProtect
-from forms import LoginForm
+from flask import Flask, render_template
 
 app = Flask(__name__)
-app.config["SECRET_KEY"] = "supersecretkey"  # ใช้สำหรับป้องกัน CSRF
-csrf = CSRFProtect(app)
-
-
-@app.route("/login", methods=["GET", "POST"])
-def login():
-    form = LoginForm()
-    if form.validate_on_submit():
-        username = form.username.data
-        password = form.password.data
-        if username == "admin" and password == "1234":  # ตัวอย่าง Login ง่ายๆ
-            flash("Login Successful!", "success")
-            return redirect(url_for("home"))
-        else:
-            flash("Invalid username or password", "danger")
-    return render_template("login.html", form=form)
 
 
 @app.route("/")
 def home():
-    return "Hello, Recipe Manager! <br> <a href='/login'>Login</a>"
+    return render_template("index.html")
+
+
+@app.route("/login")
+def login():
+    return render_template("login.html")
 
 
 if __name__ == "__main__":
